90e4d212e214b94d299bc3415e01b47e9c74b5b0
GOKHAN6666 <gokhanalic66@gmail.com>
2025-06-01 10:58:20 +0300

    Fix environment variable name for Supabase Anon Key

--- supabaseclient.js
+++ supabaseclient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 // Ortam değişkenlerini kullanarak Supabase URL ve Anahtarını alın
 // Bu, Vercel'de ayarladığınız VITE_SUPABASE_URL ve VITE_SUPABASE_ANON_KEY'i kullanmasını sağlayacak
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
 // Eğer geliştirme ortamında (localhost) çalışıyorsanız ve ortam değişkenleri tanımlı değilse
 // yedek olarak sabit değerler kullanabilirsiniz, ancak bu sadece geliştirme içindir.
@@ -13,8 +13,7 @@ if (!supabaseUrl || !supabaseKey) {
   // Alternatif olarak, burada bir hata mesajı gösterebilir veya uygulamayı başlatmayabilirsiniz.
   // Veya sadece geliştirme ortamı için buraya geçici sabit değerler koyabilirsiniz.
   // Örneğin:
-  // supabaseUrl = 'https://jqolgnlespsbtnyvqobg.supabase.co';
-  // supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxb2xnbmxlc3BzYnRueXZxb2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Mzg2NjgsImV4cCI6MjA2NDAxNDY2OH0.pPfNR2JhvHbwmfFOk0mUZeW-nSsQDUVThoDDR3gfcqg';
-}
+  // supabaseUrl = 'https://trnzalpmrhhuefolsccu.supabase.co';
+  // supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRybnphbHBtcmhodWVmb2xzY2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTMxNjcsImV4cCI6MjA2MzU4OTE2N30.aRgjvx4ENryKcHHwcUHwmMoKyOB6LbK4HYqWrDTadGI' ;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
