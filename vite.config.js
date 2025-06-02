db3f4151f1346eeca609b07d771677782634ff40
GOKHAN6666 <gokhanalic66@gmail.com>
2025-06-02 13:20:51 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-// vite.config.js
-export default {
+// alicci/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Bu satırı ekleyin veya güncelleyin
+  },
   server: {
     fs: {
       strict: false,
     },
   },
-}
+});
