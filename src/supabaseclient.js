8914fe5ceedd834a3a8e780efc5beac4a36ecdb2
GOKHAN6666 <gokhanalic66@gmail.com>
2025-07-07 13:17:49 +0300

    Add supabaseclient.js

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -8,3 +8,5 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('Supabase KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
