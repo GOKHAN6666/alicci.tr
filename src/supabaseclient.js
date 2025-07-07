36b8470033a0aef4f58a3d4d0844814ebc5e85ef
GOKHAN6666 <gokhanalic66@gmail.com>
2025-07-07 13:43:18 +0300

    Supabase client güncellendi

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,17 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Ortam değişkenlerinden değerleri al
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// Console çıktısı: ENV değerleri geldi mi?
-console.log('Supabase URL:', `"${supabaseUrl}"`);
-console.log('Supabase KEY:', `"${supabaseAnonKey}"`);
-
-// Env değerleri yoksa hata fırlat
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL veya Key eksik. .env dosyanı kontrol et.');
-}
-
-// Supabase client yarat
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  "https://jqolgnlespsbtnyvqobg.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxb2xnbmxlc3BzYnRueXZxb2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Mzg2NjgsImV4cCI6MjA2NDAxNDY2OH0.pPfNR2JhvHbwmfFOk0mUZeW-nSsQDUVThoDDR3gfcqg"
+);
