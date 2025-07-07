4402a4ebb45e5732c42ba3d7ff7bd2d96f5730b2
GOKHAN6666 <gokhanalic66@gmail.com>
2025-07-07 13:20:50 +0300

    Add supabaseclient.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Gerekirse ek yapılandırmalar buraya eklenebilir
-  // Örneğin, sunucu portu veya proxy ayarları
   server: {
-    // Portu belirtebilirsiniz, varsayılan 5173'tür
-    // port: 3000,
-    host: true // Vercel'in network erişimine izin verir
+    host: true,
   },
-  // Vercel gibi platformlarda doğru yol çözünürlüğü için base yolu ayarı
   base: '/',
-  // Ortam değişkenleri için özel bir yapılandırma genellikle gerekmez.
-  // Vite, VITE_ ile başlayan değişkenleri otomatik olarak algılar.
+  define: {
+    'process.env': {
+      VITE_SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+      VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
+    },
+  },
 });
