c7e54b4897f12a59a77a266980e783422c060d8e
GOKHAN6666 <gokhanalic66@gmail.com>
2026-07-06 16:07:38 +0300

    Update supabaseclient.js

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Bilgileri elle yazmak yerine .env dosyasından ve Vercel'den otomatik çekiyoruz
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// URL'nin sonundaki "/rest/v1/" kısmını sildik, sadece temiz hali kalmalı:
+const supabaseUrl = "https://uunncptklxipioiwgbav.supabase.co";
+
+// Bu ekranın hemen altında "Project API keys" kısmında "anon public" yazan anahtarı buraya yapıştır:
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1bm5jcHRrbHhpcGlvaXdnYmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMjIzMjgsImV4cCI6MjA5ODg5ODMyOH0.i_jTX4gthlpMTCqAIHFDiMeuMEiUD4UI2SeraTT04tA"; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
