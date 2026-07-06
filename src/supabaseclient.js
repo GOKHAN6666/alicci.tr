2698c5b9c299fc3119099018e9cc15a58846cea6
GOKHAN6666 <gokhanalic66@gmail.com>
2026-07-06 12:54:58 +0300

    Update Supabase client URL and key

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  "https://jqolgnlespsbtnyvqobg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impxb2xnbmxlc3BzYnRueXZxb2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Mzg2NjgsImV4cCI6MjA2NDAxNDY2OH0.pPfNR2JhvHbwmfFOk0mUZeW-nSsQDUVThoDDR3gfcqg"
+  "https://uunncptklxipioiwgbav.supabase.co/rest/v1/",
+  "sb_publishable_kJGVQBoA6AdBwSmze_bv7A_KkRUeLIN"
 );
