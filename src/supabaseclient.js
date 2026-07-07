f888bef44b85bf24a15807f01cd7b20019af6c6f
GOKHAN6666 <gokhanalic66@gmail.com>
2026-07-07 15:57:02 +0300

    Update supabaseclient.js

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // URL'nin sonundaki "/rest/v1/" kısmını sildik, sadece temiz hali kalmalı:
-const supabaseUrl = "https://uunncptklxipioiwgbav.supabase.co/rest/v1/";
+const supabaseUrl = "https://uunncptklxipioiwgbav.supabase.co";
 
 // Bu ekranın hemen altında "Project API keys" kısmında "anon public" yazan anahtarı buraya yapıştır:
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1bm5jcHRrbHhpcGlvaXdnYmF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMjIzMjgsImV4cCI6MjA5ODg5ODMyOH0.i_jTX4gthlpMTCqAIHFDiMeuMEiUD4UI2SeraTT04tA"; 
